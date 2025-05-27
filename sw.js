4724ba5f01e42d8cb7936bcf58adbe89a8e47db3
So Miyagawa <runa.uei@gmail.com>
2025-05-27 22:44:40 +0900

    added

---
+++ sw.js
@@ -0,0 +1,47 @@
+// Service Worker for Coptic Learning PWA
+const CACHE_NAME = 'coptic-learning-v1';
+const urlsToCache = [
+    './',
+    './index.html',
+    'https://github.com/CopticScriptorium/coptic-nlp/raw/refs/heads/master/antinoou-webfont.ttf'
+];
+
+// Install event
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// Fetch event
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
+
+// Activate event
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
